d575e0e682e246ecbb398ab3e023d775fe8def3b
IT-IT-IT <adamhacham2@gmail.com>
2021-10-24 22:50:28 +0200

    Question 9 - Fin Q9

--- dm_sage.py
+++ dm_sage.py
@@ -22,4 +22,29 @@ def tronc(M):
         T.append(x)
         x = x>>1
     T.append(x)
-    print(T)
+    T.reverse()
+    return T
+
+def calcul_x(x1, M, N):
+    List = tronc(M)
+    d = dict([("1", x1)])
+    ex = 1
+    size1 = log(M,2)
+    i = floor(size1)
+    ii = 0
+    while i > 0:
+        i = i-1
+        if ((M>>i) & 1) == 0 :
+            d[str(List[ii+1])] = 2*d[str(List[ii])]**2 - 1
+            d[str(List[ii+1]+1)] = 2*d[str(List[ii])]*d[str(List[ii]+1)] - x1
+            print(d)
+        if ((M>>i) & 1) == 1:
+            d[str(List[ii+1])] = Integer(2)*d[str(List[ii])]*d[str(List[ii]+1)] - x1
+            d[str(List[ii+1]+1)] = 2*d[str(List[ii])]**2 - 1
+            print(d)
+
+        ii = ii + 1            
+#        ex = l
+#        z1 = 2*Xi[1]*Xi[l+1] - x1
+    print(d)
+#        print(z1)
